46cb92b96bb8c91346e036ccec3aa3a567063e48
Matt <mattkubota5@gmail.com>
2018-02-07 18:42:34 -0500

    created a sessions controller and started working on sign in functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'toots#index'
-  get '/sign_up', to: 'users#new'
-  # get '/log_in', to: 'sessions#new'
   resources :toots
   resources :users, except: :new
+  get '/sign_up', to: 'users#new'
+  get '/log_in', to: 'sessions#new'
+  get '/log_in', to: 'sessions#create'
+  get '/log_out', to: 'sessions#destroy'
 end
