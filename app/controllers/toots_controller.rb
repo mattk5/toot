86ec728721024a1db7aec5d0bea0047bdb9f3b19
Matt <mattkubota5@gmail.com>
2018-02-16 14:44:50 -0500

    checking for errors.

--- app/controllers/toots_controller.rb
+++ app/controllers/toots_controller.rb
@@ -1,9 +1,24 @@
 class TootsController < ApplicationController
   def index
-    @toots = Toot.all 
+    @toots = Toot.all
   end
 
   def show
 
   end
+
+  def new
+    @toot = Toot.new
+  end
+
+  def create
+      @toot = Toot.new(toot_params)
+  end
+
+  private
+
+  def toot_params
+    params.require(:toot).permit(:author, :body)
+  end
+
 end
