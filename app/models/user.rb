a14f4790f106f666d4b4cf911668061a8844a00b
Matt <mattkubota5@gmail.com>
2018-02-21 16:24:08 -0500

    created associations between users and relationship models. added follow/unfollow methods to be used later.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   has_many :toots, dependent: :destroy
   has_many :comments
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower 
 
   has_secure_password
 
@@ -37,5 +41,17 @@ class User < ApplicationRecord
 
   def feed
       Toot.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
