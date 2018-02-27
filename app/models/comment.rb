9f1ae8595dc3ef1f0ed67f10ef7086e43a80b81a
Matt <mattkubota5@gmail.com>
2018-02-27 16:33:18 -0500

    fixed models to ensure proper connection between model objects.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymophic: true 
+  belongs_to :commentable, polymorphic: true
 
   validates :body, presence: true, uniqueness: true, length: { maximum: 280 }
-  validates :user_id, presence: true
-  validates :toot_id, presence: true
 end
