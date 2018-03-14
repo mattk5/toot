33fb8764fd543aef4a8dd3cc2ad9be19f735e483
Matt <mattkubota5@gmail.com>
2018-03-13 23:34:42 -0400

    added follow/unfollow functionality, ability to see followers/following

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :users do
     resources :comments
   end
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships 
   get '/sign_up', to: 'users#new'
   get '/log_in', to: 'sessions#new'
   post '/log_in', to: 'sessions#create'
